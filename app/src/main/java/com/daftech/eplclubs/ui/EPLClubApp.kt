94991a0a7218bad84157c3ac8b305c5da1642dbc
dintaaditya <dintaaditya27@gmail.com>
2024-09-13 16:36:36 +0700

    setup navigation

---
+++ app/src/main/java/com/daftech/eplclubs/ui/EPLClubApp.kt
@@ -0,0 +1,110 @@
+package com.daftech.eplclubs.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.daftech.eplclubs.R
+import com.daftech.eplclubs.ui.navigation.NavigationItem
+import com.daftech.eplclubs.ui.navigation.Screen
+import com.daftech.eplclubs.ui.screen.about.AboutScreen
+import com.daftech.eplclubs.ui.screen.favorite.FavoriteScreen
+import com.daftech.eplclubs.ui.screen.home.HomeScreen
+
+@Composable
+fun EPLClubApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+    Scaffold(
+        bottomBar = {
+            BottomBar(navController)
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+            composable(Screen.Favorite.route) {
+                FavoriteScreen()
+            }
+            composable(Screen.About.route) {
+                AboutScreen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        val navigationItems = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite,
+                screen = Screen.Favorite
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_about),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.About
+            ),
+        )
+
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        navigationItems.map { item ->
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.title
+                    )
+                },
+                label = { Text(item.title) },
+                selected = currentRoute == item.screen.route,
+                onClick = {
+                    navController.navigate(item.screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        restoreState = true
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/daftech/eplclubs/ui/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.daftech.eplclubs.ui.navigation
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Favorite : Screen("favorite")
+    object About : Screen("about")
+    object DetailClub : Screen("home/{clubId}") {
+        fun createRoute(clubId: Long) = "home/$clubId"
+    }
+}
